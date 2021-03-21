d2a7e50f4e6532242e24069faa03f07202169800
HOME-PC\Phalanaxis <firekeeperm@gmail.com>
2021-03-21 17:59:58 +0400

    changes in 2.1.1

--- Task 2/Task 2.1/Task 2.1.1/Task 2.1.1/Program.cs
+++ Task 2/Task 2.1/Task 2.1.1/Task 2.1.1/Program.cs
@@ -27,8 +27,28 @@ namespace Task_2._1._1
 
         }
 
-        public void concatenation()
-        { }
+        public char[] concatenation(char[] ar)
+        {
+            symbol = symbol.Concat(ar).ToArray();
+            return symbol;
+        }
+
+        public MyString FromCharArray(char[] array)
+        {
+            symbol = array;
+
+            return this;
+        }
+
+        public char[] ConvertToCharArr()
+        {
+            return symbol;
+        }
+
+        public int Length()
+        {
+            return this.symbol.Length;
+        }
 
         public void Search() //поиск первого вхождения символа в массив, возможно не понял задание и нужно было что-то другое
         {
@@ -36,10 +56,6 @@ namespace Task_2._1._1
             Console.WriteLine("введите искомый символ:  ");
             char.TryParse(Console.ReadLine(), out char value);
             Console.WriteLine(Array.IndexOf(symbol, value));
-        }
-
-        public void ConvertToCharArr()
-        {
         }
 
         public void PrintResult()
@@ -63,6 +79,11 @@ namespace Task_2._1._1
             Initial.PrintResult();
             Initial.Search();
             Initial.Comparison();
+            Console.WriteLine($"Длина строки {Initial.Length()}");
+
+            //MyString second = new MyString(Console.ReadLine());
+            Console.WriteLine(Initial.concatenation("slovo".ToCharArray()));
+           
         }
     }
 }
